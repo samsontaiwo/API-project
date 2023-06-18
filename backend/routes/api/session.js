fee0b0baf5824aa852fd64b8cf87cb9c18dad37e
samsontaiwo <tywo389@gmail.com>
2023-06-18 15:41:34 -0500

    Add User signup backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -44,6 +44,28 @@ router.post(
     }
   );
 
+  // Sign up
+router.post(
+    '/',
+    async (req, res) => {
+      const { email, password, username } = req.body;
+      const hashedPassword = bcrypt.hashSync(password);
+      const user = await User.create({ email, username, hashedPassword });
+  
+      const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+      };
+  
+      await setTokenCookie(res, safeUser);
+  
+      return res.json({
+        user: safeUser
+      });
+    }
+  );
+
   router.delete(
     '/',
     (_req, res) => {
