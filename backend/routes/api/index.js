6f462336219c7492dd98836539a6ffd8c1a03ffb
samsontaiwo <tywo389@gmail.com>
2023-06-18 23:54:38 -0500

    Add booking router

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
+const bookingsRouter = require('./bookings.js');
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
 // const { User } = require('../../db/models');
 
@@ -27,6 +28,8 @@ router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 
 router.use('/spots', spotsRouter);
+
+router.use('/bookings', bookingsRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
